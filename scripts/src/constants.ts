ecaad1f72ee4cd3b4e63ca19d0a69b07c9b8e8a8
Ramzi <manager.s.ram@gmail.com>
2026-03-21 07:17:13 +0100

    Deleted call-stripe-debug.sh

--- scripts/src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const HOME_RECENT_LIMIT = 5;
--- scripts/src/lib/getCompanyId.ts
+++
@@ -1,4 +0,0 @@
-export function getCompanyId(): string | null {
-  const companyId = localStorage.getItem('selected_company_id');
-  return companyId;
-}
