ecf57a6ee84d5e9a0c141f515027607a43073933
Ramzi <manager.s.ram@gmail.com>
2026-04-14 17:51:50 +0200

    Deleted App copy.tsx

--- src/lib/useCurrentCompany.ts
+++
@@ -1,6 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-export function useCurrentCompany(): string | null {
-  const { companyId } = useParams<{ companyId: string }>();
-  return companyId || null;
-}
